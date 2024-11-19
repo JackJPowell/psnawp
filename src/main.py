84c1e40a9ca08581615c9df8a0d770f65f529990
Jack Powell <jackattack51@gmail.com>
2024-11-18 19:55:21 -0500

    Remove unnecessary dependencies

--- src/main.py
+++ src/main.py
@@ -5,9 +5,13 @@ psnawp = PSNAWP("")
 
 # Your Personal Account Info
 client = psnawp.me()
+
+me = psnawp.user(online_id='me')
+print(me.profile())
+
 print(f"Online ID: {client.online_id}")
 print(f"Account ID: {client.account_id}")
 title = psnawp.game_title("PPSA21564_00", "me")
 info = title.get_details()
-#german = title.get_details("DE", "de")
-print(info)
+german = title.get_details("ZZ", "zz")
+#print(info)
